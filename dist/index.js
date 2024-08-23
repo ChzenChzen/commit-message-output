9399f7d525b626e34f9ea36733611c5ea293ba37
ChzenChzen <fedorovdanila@me.com>
2024-08-23 21:22:49 +0500

    Update action to log commit message
    Simplified the GitHub Action script by removing the greeting logic and unused payload logging. Now, it captures and logs the commit message from the current GitHub context.

--- dist/index.js
+++ dist/index.js
@@ -31095,14 +31095,11 @@ const core = __nccwpck_require__(6150);
 const github = __nccwpck_require__(6557);
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const defaultValue = core.getInput('default');
+    const commitMessage = github.context.payload.head_commit.message;
+    console.log(commitMessage);
+    // core.setOutput("tag", commitMessage);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
