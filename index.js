f00fdeb18041c933cdf42323a59f4d02d5586c89
ChzenChzen <fedorovdanila@me.com>
2024-08-23 21:25:38 +0500

    minor fixes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ try {
     const commitMessage = github.context.payload.head_commit.message;
     console.log(commitMessage);
     // core.setOutput("tag", commitMessage);
-    // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
     core.setFailed(error.message);
 }
