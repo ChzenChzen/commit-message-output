85f7f02cedfaf3d31621ba0b83b8cfa2fab6bf3f
ChzenChzen <fedorovdanila@me.com>
2024-08-23 21:20:29 +0500

    Update action to process commit messages
    Replaced greeting functionality with commit message parsing. Adjusted inputs and outputs to handle default values and parsed tags from commit messages.

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const defaultValue = core.getInput('default');
+    const commitMessage = github.context.payload.head_commit.message;
+    console.log(commitMessage);
+    // core.setOutput("tag", commitMessage);
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
